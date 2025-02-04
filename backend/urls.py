60f348604cf150258ec591209088c30cd19e4535
karungop <karun.gopal@gmail.com>
2025-02-03 20:39:19 -0500

    updating dependencies

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include your app's URLs
+    path('api/', include('api.urls')),
+    path('/')  # Include your app's URLs
 ]
 
